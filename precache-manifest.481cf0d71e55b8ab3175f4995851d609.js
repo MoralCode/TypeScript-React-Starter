671979684c50b38c223a1c96e805febdf9fa8e75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-25 00:03:19 +0000

    Deploy MoralCode/TypeScript-React-Starter to github.com/MoralCode/TypeScript-React-Starter.git:gh-pages

---
+++ precache-manifest.481cf0d71e55b8ab3175f4995851d609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc8a3da765b9db734abb3bc73628bda",
+    "url": "/TypeScript-React-Starter/index.html"
+  },
+  {
+    "revision": "558646be82d857849fea",
+    "url": "/TypeScript-React-Starter/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d9931b2256b6479c216c",
+    "url": "/TypeScript-React-Starter/static/js/2.1ebe39b8.chunk.js"
+  },
+  {
+    "revision": "558646be82d857849fea",
+    "url": "/TypeScript-React-Starter/static/js/main.fb6d4cec.chunk.js"
+  },
+  {
+    "revision": "cb80bf1f79034e847783",
+    "url": "/TypeScript-React-Starter/static/js/runtime-main.d21c830e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TypeScript-React-Starter/static/media/logo.25bf045c.svg"
+  }
+]);
